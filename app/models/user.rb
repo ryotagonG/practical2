e6fe0070ffc7a98d736de134a5bfad7aa388c51c
ryotagonG <ryotapb715@gmail.com>
2021-09-05 10:16:08 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
