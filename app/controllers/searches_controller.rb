704f472558ccc4de563a6d27ba09af27d2b3b580
ryotagonG <ryotapb715@gmail.com>
2021-09-08 01:54:27 +0000

    updated

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,26 @@
 class SearchesController < ApplicationController
   def search
+    @model = params["model"]
+    @method = params["method"]
+    @content = params["content"]
+    @records = search_for(@model, @method, @content)
+  end
+  
+  private
+  
+  def search_for(model, method, content)
+    if model == "user"
+      if method == "perfect"
+        User.where(name: content)
+      else
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == "post"
+      if method == "perfect"
+        Book.where(title: content)
+      else
+        Book.where('title LIKE ?', '%'+content+'%')
+      end
+    end
   end
 end
